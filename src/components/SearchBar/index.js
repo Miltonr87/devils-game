a7565d933dc03eb5e9aa6282c720b62d22de8cb3
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-05-06 14:51:54 -0300

    CSS margin + first component

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Searchbar() {
+    return (
+        <section className="searchbox-wrap">
+            <input type="text" placeholder="Search for a Game..." className="searchbox" />
+        </section>
+    )
+}
+
+export default Searchbar;
